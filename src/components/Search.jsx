4319d507a3668756f77e88aa0951cf3dd18a1db3
deyvid-dev <deyvidscm@hotmail.com>
2021-08-09 16:41:13 -0300

    Requisito2 completo

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h3>
+      </div>
+    );
+  }
+}

e70ca31f4ab9ebe81f2fdf3bf6a53824c51029d6
Micael Santos <micaelmaicon@hotmail.com>
2021-08-09 15:45:21 -0300

    Iniciando PROJETO - Grupo 24

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 export async function getCategories() {
   // Implemente aqui
   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((resp) => resp.json());
+    .then((response) => response.json());
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {

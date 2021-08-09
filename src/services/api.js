650a3c28b2013c23ae0159cba535031968a6c5cc
Murilo Rainho <murilorainho01@gmail.com>
2021-08-09 16:57:30 -0300

    req-01 completo

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
-  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((res) => res.json());
+  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const json = await request.json();
+  return json;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
+  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+  const json = await request.json().results;
+  return json;
 }

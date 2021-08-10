a4175f13635612f2f9001d8c6e820b0155a9d743
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-08-10 12:37:36 -0300

    RInicio req.3

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CartButton from './CartButton';
 
 export default class Search extends React.Component {
   render() {
@@ -7,6 +8,7 @@ export default class Search extends React.Component {
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
+        <CartButton />
       </div>
     );
   }
